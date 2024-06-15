ec395c06e046c379dc8f68500de7ccd13726eb51
Narmathaa JB <balanarmathaa@gmail.com>
2024-06-15 15:08:10 +0530

    sidebar

--- happytales/lib/Screens/Welcome Screen/welcome_screen.dart
+++ happytales/lib/Screens/Welcome Screen/welcome_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:happytales/Screens/Home/home_screen.dart';
 import 'package:happytales/Screens/Welcome%20Screen/intro_screens/intro_page1.dart';
 import 'package:happytales/Screens/Welcome%20Screen/intro_screens/intro_page2.dart';
 import 'package:happytales/Screens/Welcome%20Screen/intro_screens/intro_page3.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
@@ -67,7 +65,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           ),
         ),
         // indicator
-       
+
     ],
     ));
   }
