66d378ea71dc08f9291cb2196c5049bbe12fa3f1
Narmathaa JB <balanarmathaa@gmail.com>
2024-06-25 22:19:57 +0530

    HomePage

--- happytales/lib/Screens/Home/home_screen.dart
+++ happytales/lib/Screens/Home/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:happytales/Screens/Home/maintab_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,12 +10,36 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  List topPicksArr =[
+    {
+      "name" : "The Tortoise and the Hare",
+      "author": "Aesop",
+      "img":""
+    },
+    {
+      "name" : "The Lion and the Mouse",
+      "author": "Aesop, Bernadette Watts",
+      "img":""
+    },
+    {
+      "name" : "Goodnight Moon",
+      "author": "Margaret Wise Brown",
+      "img":""
+    },
+    {
+      "name" : "The Three Little Pigs",
+      "author": "James Halliwell-Phillipps",
+      "img":""
+    },
+  ];
   @override
   Widget build(BuildContext context) {
 
     var media = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white,
+      
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -34,9 +60,63 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     ),
                   ),
+                ),
+
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+
+                  children: [
+                    AppBar(
+                      backgroundColor: Colors.transparent,
+                      elevation: 0,
+                      title: Row(
+                        children: [
+                          Text(
+                            "Our Top Picks",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 22,
+                              fontWeight: FontWeight.w700
+                              ),
+                            ),
+                        ],
+                      ),
+                      leading: Container(),
+                      leadingWidth: 1,
+                      actions: [
+                        IconButton(
+                          onPressed: (){}, icon: Icon(Icons.menu)
+                        )
+                      ],
+                    ),
+
+                    Container(
+                      child: CarouselSlider.builder(
+                        itemCount: topPicksArr.length,
+                        itemBuilder: (BuildContext context, int itemIndex, int pageViewIndex) {
+                          return Container();
+
+                        },
+                        options: CarouselOptions(
+                          autoPlay: true,
+                          enlargeCenterPage: true,
+                          viewportFraction: 0.6,
+                          aspectRatio: 0.9,
+                          enlargeFactor: 0.4,
+                          enlargeStrategy: CenterPageEnlargeStrategy.height,
+                        ),
+                      
+                      ),
+                    )
+                  ],
                 )
               ],
-            )
+            ),
+            SizedBox(
+            height: 505, // Adjust the height as needed
+            child: MaintabBar(),
+          ),
           ],
         ),
       ),
--- happytales/lib/Screens/Home/maintab_bar.dart
+++ happytales/lib/Screens/Home/maintab_bar.dart
@@ -7,9 +7,56 @@ class MaintabBar extends StatefulWidget {
   State<MaintabBar> createState() => _MaintabBarState();
 }
 
-class _MaintabBarState extends State<MaintabBar> {
+class _MaintabBarState extends State<MaintabBar>with TickerProviderStateMixin {
+  
+  TabController? controller;
+
   @override
+
+  @override
+  void initState() {
+    controller = TabController(length: 4, vsync: this);
+    super.initState();
+    
+  }
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: TabBarView(
+        controller: controller,
+        children: [
+          Container(),
+          Container(),
+          Container(),
+          Container(),
+        ]
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Color.fromRGBO(111, 175, 227, 1),
+        child: TabBar(
+          controller: controller,
+
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.white54,
+          tabs: [
+            Tab(
+              icon: Icon(Icons.home),
+              text: "Home",
+            ),
+            Tab(
+              icon: Icon(Icons.create),
+              text: "Create",
+            ),
+            Tab(
+              icon: Icon(Icons.library_books_rounded),
+              text: "Stories",
+            ),
+            Tab(
+              icon: Icon(Icons.settings),
+              text: "Settings",
+            ),
+          ]
+        ),
+      ),
+    );
   }
 }
