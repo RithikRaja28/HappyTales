3ab3e3a346a7cd273b4266e6f9441cc7e4072e59
Narmathaa JB <balanarmathaa@gmail.com>
2024-08-11 19:10:50 +0530

    Gemini

--- happytales/lib/Screens/Gemini API/gemini.dart
+++ happytales/lib/Screens/Gemini API/gemini.dart
@@ -1,17 +1,171 @@
 import 'package:flutter/material.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:happytales/Screens/Gemini%20API/message_widget.dart';
 
-class Gemini extends StatelessWidget {
+class Gemini extends StatefulWidget {
   const Gemini({super.key});
+
+  @override
+  State<Gemini> createState() => _GeminiState();
+}
+
+class _GeminiState extends State<Gemini> {
+  late final GenerativeModel _model;
+  late final ChatSession _chatSession;
+  final FocusNode _textFieldFocus = FocusNode();
+  final TextEditingController _textController = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
+  bool _loading = false;
+
+
+  @override
+  void initState(){
+    super.initState();
+    _model = GenerativeModel(
+      model: 'gemini-pro',
+      apiKey: const String.fromEnvironment('api_key'),
+    );
+    _chatSession = _model.startChat();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Gemini Page'),
+        title: const Text('Gemini Page'),
       ),
-      body: Center(
-        child: Text('Welcome to the Gemini Page!'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: ListView.builder(
+                controller: _scrollController,
+                itemCount: _chatSession.history.length,
+                itemBuilder: (context, index){
+                  final Content content = _chatSession.history.toList()[index];
+                  final text = content.parts.whereType<TextPart>().map<String>((e) => e.text).join('');
+                  return MessageWidget(
+                    text: text,
+                    isFromUser: content.role == 'user'
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+              vertical: 25,
+              horizontal: 15,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      autofocus: true,
+                      focusNode: _textFieldFocus,
+                      decoration: textFieldDecoration(),
+                      controller: _textController,
+                      onSubmitted: _sendChatMessage,
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                ],
+              ),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+
+  InputDecoration textFieldDecoration() {
+    return InputDecoration(
+      contentPadding: const EdgeInsets.all(15),
+      hintText: 'Enter a prompt...',
+      border: OutlineInputBorder(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(14),
+        ),
+        borderSide: BorderSide(
+          color: Theme.of(context).colorScheme.secondary,
+        ),
       ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(14),
+        ),
+        borderSide: BorderSide(
+          color: Theme.of(context).colorScheme.secondary,
+        )
+      ),
+    );
+  }
+
+  Future<void> _sendChatMessage(String message) async{
+    setState((){
+      _loading = true;
+    });
+    try{
+      final response = await _chatSession.sendMessage(
+        Content.text(message),
+      );
+      final text = response.text;
+      if(text == null){
+        _showError('No response from API.');
+        return;
+      }
+      else{
+        setState(() {
+          _loading = false;
+          _scrollDown();
+        });
+      }
+    } catch (e){
+      _showError(e.toString());
+      setState(() {
+        _loading = false;
+      });
+    } finally {
+      _textController.clear();
+      setState(() {
+        _loading = false;
+      });
+      _textFieldFocus.requestFocus();
+    }
+  }
+
+  void _scrollDown(){
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) => _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(
+          milliseconds: 750,
+        ),
+        curve: Curves.easeOutCirc,
+      ),
+    );
+  }
+
+  void _showError(String message){
+    showDialog<void>(
+      context: context,
+      builder: (context){
+        return AlertDialog(
+          title: const Text('Something went wrong'),
+          content: SingleChildScrollView(
+            child: SelectableText(message),
+          ),
+          actions: [
+            TextButton(
+              onPressed: (){
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'))
+          ],
+        );
+      }
     );
   }
 }
---
+++ happytales/lib/Screens/Gemini API/message_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+
+class MessageWidget extends StatelessWidget {
+  const MessageWidget({
+    super.key,
+    required this.text,
+    required this.isFromUser,
+  });
+
+  final String text;
+  final bool isFromUser;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: isFromUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        Flexible(
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 15,
+              horizontal: 20,
+            ),
+            margin: const EdgeInsets.only(bottom: 8),
+            constraints: const BoxConstraints(maxWidth: 520),
+            decoration: BoxDecoration(
+              color: isFromUser
+                  ? Theme.of(context).colorScheme.primary
+                  : Theme.of(context).colorScheme.secondary,
+              borderRadius: BorderRadius.circular(18),
+            ),
+            child: Column(
+              children: [
+                MarkdownBody(data: text),
+              ],
+            ),
+          )
+        )
+      ],
+    );
+  }
+}
